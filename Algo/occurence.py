891623d72a8e87e1306c958a20c086195ad5e074
Trivador <eliotcas05@gmail.com>
2022-01-14 22:19:03 +0100

    Add occurence.py

---
+++ Algo/occurence.py
@@ -0,0 +1,10 @@
+# Programe par Eliot CASSIDY
+
+def occurence(values: list, arg) -> bool:
+    """ Renvoie True si arg dans values ou False sinon"""
+    assert isinstance(values, list)
+
+    for i in range(len(values)):
+        if values[i] == arg:
+            return True
+    return False
